d52d47be467119ccd3955d3293655ea07fa4d2cf
Arik-K <arioar20@gmail.com>
2024-07-02 19:56:09 +0300

    Texntures and first Game maze

--- ClashVRSenseMaze/Assets/Scripts/PauseScreen.cs
+++ ClashVRSenseMaze/Assets/Scripts/PauseScreen.cs
@@ -9,9 +9,9 @@ using System.Runtime.CompilerServices;
 
 public class PauseScreen : MonoBehaviour
 {
-    public InputActionReference OpenMenuAction;
+    public InputActionProperty OpenMenuAction;
     public GameObject vPauseMenu;
-    public GameObject NextLevelCanvas;
+    public GameObject menuCanvas;
     public bool isPaused;
 
     //public GameManagerScript gms;
@@ -21,84 +21,14 @@ public class PauseScreen : MonoBehaviour
     //private string[] sample_start = {""};
     
     // Define Toggle Button for Menu
-    void Awake()
-    {
-        OpenMenuAction.action.Enable();
-        OpenMenuAction.action.performed -= ToggleMenu;
-    }
 
-    private void OnDestroy()
+    void Update()
     {
-        OpenMenuAction.action.Disable();
-        OpenMenuAction.action.performed -= ToggleMenu;
-    }
-
-    private void ToggleMenu(InputAction.CallbackContext context)
-    {
-        if (isPaused)
+        if(OpenMenuAction.action.WasPressedThisFrame())
         {
-            ResumeGame();
-        }
-        else
-        {
-            PauseGame();
+            menuCanvas.SetActive(!menuCanvas.activeSelf);
+            vPauseMenu.SetActive(true);
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        vPauseMenu.SetActive(false);
-        NextLevelCanvas.SetActive(false);
-        //gms = FindObjectOfType<GameManagerScript>();
-        //ts = FindObjectOfType<TrainingScript>();
-       //mm = FindObjectOfType<MazeManager>();
-    }
-
-    // Update is called once per frame
-
-    public void QuitGame()
-    {
-         Application.Quit();
-    }
-
-    public void PauseGame()
-    {
-        vPauseMenu.SetActive(true);
-        Time.timeScale = 0f;
-        isPaused = true;  
-    }
-
-    public void ResumeGame()
-    {
-        vPauseMenu.SetActive(false);
-        Time.timeScale = 1f;
-        isPaused = false;  
-  
-    }
-
-        public void GoToMainMenu()
-    {
-         Time.timeScale = 1f;
-         SceneManager.LoadScene("MainMenu");   
-    }
-
-    public void NextMazeButton()
-    {
-        //mm.TriggerRotationStop();
-        //if(MainMenu.isTraining)
-            {
-                //ts.NextMaze();
-                AudioListener.volume = 1f;
-                NextLevelCanvas.SetActive(false);
-                
-            }
-       // else
-            {
-                //gms.NextMaze();
-                AudioListener.volume = 1f;
-                NextLevelCanvas.SetActive(false);
-
-            }
-    }
 }
